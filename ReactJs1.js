5fe30cd54e7a3785b21ad654f25f4c096c56e02f
Siddharth6937 <siddharth6937@gmail.com>
2024-04-23 18:38:51 +0530

    React intro

---
+++ ReactJs1.js
@@ -0,0 +1,9 @@
+/*React
+    - is all about components
+    - What to do! Not how to do!
+
+    - SPA :- Single Page Application (Single html page, rest everything controlled by React.js library)
+            ( API :- Application programming Interface )
+    -  Uses Virtual DOM        
+
+*/

b9dd27b9697dec819fb8451c5d37ac150c7a4473
Siddharth6937 <siddharth6937@gmail.com>
2024-04-28 00:13:36 +0530

    ReactJs index.js components Types Features, JSX, JSON/XML, Fragements

---
+++ ReactJS-1/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      We are learing React
+    </div>
+    <div className="App">React is Osm</div>
+    </>
+  );
+}
+
+
+export default App;
+
+/*
+    Components - 
+    types :-
+    1. Class Based Components
+    2. Function Based Componets
+
+    - Components can be considered as sub-section inside the entire application
+    - Return JSX (JavaScript + XML)
+    - Can be Nested
+    - Can be Re-Used
+
+    Fragments :-Empty JSX tags (used for nesting multiple elements inside a single parent to be returned by the component)
+
+
+    XML = Extensible Markup Language 
+*/

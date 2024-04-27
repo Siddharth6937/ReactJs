87775a40927ce9b79ba56328d87f84868b691b58
Siddharth6937 <siddharth6937@gmail.com>
2024-04-28 00:15:13 +0530

    ReactJS Intro

--- ReactJs1.js
+++ ReactJs1.js
@@ -4,6 +4,13 @@
 
     - SPA :- Single Page Application (Single html page, rest everything controlled by React.js library)
             ( API :- Application programming Interface )
-    -  Uses Virtual DOM        
+    -  Uses Virtual DOM     
+    
+    - create-react-app
+
+    - npm / npx
+
+    - npx create-react-app react-js-1
 
 */
+console.log("hello") 
